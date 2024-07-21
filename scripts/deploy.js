3bb3027b93786aff5a59b8ca92a0d3442eb7fdf1
Tundex1 <91364830+Tundex1@users.noreply.github.com>
2024-07-21 20:21:30 +0100

    first message

---
+++ scripts/deploy.js
@@ -0,0 +1,11 @@
+const { ethers } = require("ethers");
+
+async function main() {
+    // Logic to deploy smart contracts
+    console.log("Deploying contracts...");
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
